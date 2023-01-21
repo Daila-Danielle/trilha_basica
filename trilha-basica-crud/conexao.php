b2386cb98195c7b856b1380801ad60310c18cff5
Daila-Danielle <dailasantos565@gmail.com>
2023-01-21 15:31:13 -0300

    teste

--- trilha-basica-crud/conexao.php
+++ trilha-basica-crud/conexao.php
@@ -1,8 +1,8 @@
 <!-- faz a conexao com o banco de dados -->
 <?php
-    define('HOST', '192.168.1.254');
-    define('USER', 'estagiario');
-    define('PASS', 'estagio123');
+    define('HOST', '127.0.0.1');
+    define('USER', 'root');
+    define('PASS', '');
     define('BASE', 'treinamento_daila');
 
     $conexao = new MySQLi(HOST, USER, PASS, BASE);
